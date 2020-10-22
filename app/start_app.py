0c500799a4531e4fe67c2c859a354716d146e953
tojatos <tojatos@gmail.com>
2020-10-22 18:17:07 +0200

    Added api for getting the mails

--- app/start_app.py
+++ app/start_app.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask, render_template, Response, jsonify
+from flask import Flask, request, jsonify
+from operator import itemgetter
+from app.edu_cl_mail import get_mails
 
 HOST = '0.0.0.0'
 PORT = 80
@@ -7,9 +9,14 @@ DEBUG_PORT = 8099
 
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return "it works!"
+@app.route("/api/get_mails/<amount>", methods=['POST'])
+def main(amount):
+    try:
+        username, password = itemgetter('username', 'password')(request.json)
+        mails = get_mails(username, password, int(amount))
+        return jsonify(mails)
+    except:
+        return jsonify('invalid credentials or internal error')
 
 if __name__ == "__main__":
     if 'DEBUG' not in os.environ:
