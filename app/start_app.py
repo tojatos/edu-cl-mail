bbb8fd6dd3c12e69fbcea579dce3dd3fce2322ad
tojatos <tojatos@gmail.com>
2020-10-23 18:05:12 +0000

    HTTPS support and env variables added

--- app/start_app.py
+++ app/start_app.py
@@ -3,9 +3,20 @@ from flask import Flask, request, jsonify
 from operator import itemgetter
 from app.edu_cl_mail import get_mails
 
-HOST = '0.0.0.0'
-PORT = 2020
-DEBUG_PORT = 8099
+from dotenv import load_dotenv
+load_dotenv()
+
+def get_env(key, fallback):
+    try:
+        return os.environ[key]
+    except:
+        return fallback
+
+HOST = get_env('HOST', '0.0.0.0')
+PORT = get_env('PORT', 80)
+DEBUG_PORT = get_env('DEBUG_PORT', 8099)
+SSL_CERT_LOCATION = get_env('SSL_CERT_LOCATION', 'cert.pem')
+SSL_CERT_KEY_LOCATION = get_env('SSL_CERT_KEY_LOCATION', 'key.pem')
 
 app = Flask(__name__)
 
@@ -20,6 +31,7 @@ def main(amount):
 
 if __name__ == "__main__":
     if 'DEBUG' not in os.environ:
-        app.run(host=HOST, port=PORT, debug=False)
+        app.run(host=HOST, port=PORT, debug=False, ssl_context=(SSL_CERT_LOCATION, SSL_CERT_KEY_LOCATION))
     else:
         app.run(host=HOST, port=DEBUG_PORT, debug=True)
+
