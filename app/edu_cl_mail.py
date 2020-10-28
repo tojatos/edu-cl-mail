a0b1f91e5d4cdc9385960da4b6731c4b2773f251
tojatos <tojatos@gmail.com>
2020-10-28 20:34:58 +0100

    Parallelized fetching mails

--- app/edu_cl_mail.py
+++ app/edu_cl_mail.py
@@ -1,15 +1,78 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import multiprocessing
+from itertools import repeat
 
-def get_all_mails(login, password):
-    return get_mails(login, password, -1)
+try:
+    cpus = multiprocessing.cpu_count()
+except NotImplementedError:
+    cpus = 2   # arbitrary default
+
+base_url = 'https://edukacja.pwr.wroc.pl/EdukacjaWeb/studia.do'
+inbox_url = 'https://edukacja.pwr.wroc.pl/EdukacjaWeb/zawartoscSkrzynkiPocztowej.do'
+mail_content_url = 'https://edukacja.pwr.wroc.pl/EdukacjaWeb/podgladWiadomosci.do'
+
+
+def get_mail_content(row_id, s, web_session_token):
+    mail_content_params = {
+        'clEduWebSESSIONTOKEN': web_session_token,
+        'event': 'positionRow',
+        'positionIterator': 'WiadomoscWSkrzynceViewIterator',
+        'rowId': row_id,
+    }
+    mail_content_res = s.get(mail_content_url, params=mail_content_params)
+
+    mail_content_soup = BeautifulSoup(mail_content_res.content, 'html.parser')
+
+    table = mail_content_soup.find('table', {'class': 'KOLOROWA'})
+    tds = table.find_all('td', class_='BIALA')
+    content = str(tds[15])
+    return content.split('-->')[-1].replace('<br/>','').replace('</td>', '').replace('\r', '\n').strip()
+
+
+def get_five_mails(paging_range_start, s, web_token, web_session_token):
+    '''get five mails from paging_range_start'''
+    inbox_data = {
+        'cl.edu.web.TOKEN': web_token,
+        'clEduWebSESSIONTOKEN': web_session_token,
+        'pagingIterName': 'WiadomoscWSkrzynceViewIterator',
+        'pagingRangeStart': paging_range_start,
+        'event': 'positionIterRangeStart',
+    }
+
+    inbox_res = s.post(inbox_url, data=inbox_data)
+    inbox_soup = BeautifulSoup(inbox_res.content, 'html.parser')
+
+    table = inbox_soup.find('table', {'class': 'KOLOROWA'})
+    tds = table.find_all('td', class_='BIALA')
+
+    i = 0
+
+    messages_datas = []
+    temp_data = {}
+    for td in tds:
+        if i == 1:
+            temp_data['sender'] = td.text.strip()
+        if i == 2:
+            temp_data['title'] = td.text.strip()
+            row_id = re.findall(r'rowId=(.*)&',td.find('a').attrs['href'])[0]
+            temp_data['message'] = get_mail_content(row_id, s, web_session_token)
+        if i == 3:
+            temp_data['priority'] = td.text.strip()
+        if i == 4:
+            temp_data['date'] = td.text.strip()
+
+        i += 1
+        if i % 5 == 0:
+            i = 0
+            messages_datas.append(temp_data)
+            temp_data = {}
+
+    return messages_datas
 
 def get_mails(login, password, max_mails):
     '''-1 in max_mails means infinity'''
-    base_url = 'https://edukacja.pwr.wroc.pl/EdukacjaWeb/studia.do'
-    inbox_url = 'https://edukacja.pwr.wroc.pl/EdukacjaWeb/zawartoscSkrzynkiPocztowej.do'
-    mail_content_url = 'https://edukacja.pwr.wroc.pl/EdukacjaWeb/podgladWiadomosci.do'
 
     s = requests.Session()
 
@@ -37,67 +100,20 @@ def get_mails(login, password, max_mails):
     inbox_init_res = s.get(inbox_url, params=inbox_init_params)
     last_page_num = int(BeautifulSoup(inbox_init_res.content, 'html.parser').find_all('input', class_='paging-numeric-btn')[-1].get('value'))
 
-    def get_mail_content(row_id):
-        mail_content_params = {
-            'clEduWebSESSIONTOKEN': web_session_token,
-            'event': 'positionRow',
-            'positionIterator': 'WiadomoscWSkrzynceViewIterator',
-            'rowId': row_id,
-        }
-        mail_content_res = s.get(mail_content_url, params=mail_content_params)
-
-        mail_content_soup = BeautifulSoup(mail_content_res.content, 'html.parser')
-
-        table = mail_content_soup.find('table', {'class': 'KOLOROWA'})
-        tds = table.find_all('td', class_='BIALA')
-        content = str(tds[15])
-        return content.split('-->')[-1].replace('<br/>','').replace('</td>', '').replace('\r', '\n').strip()
-
-    def get_five_mails(paging_range_start):
-        '''get five mails from paging_range_start'''
-        inbox_data = {
-            'cl.edu.web.TOKEN': web_token,
-            'clEduWebSESSIONTOKEN': web_session_token,
-            'pagingIterName': 'WiadomoscWSkrzynceViewIterator',
-            'pagingRangeStart': paging_range_start,
-            'event': 'positionIterRangeStart',
-        }
-
-        inbox_res = s.post(inbox_url, data=inbox_data)
-        inbox_soup = BeautifulSoup(inbox_res.content, 'html.parser')
-
-        table = inbox_soup.find('table', {'class': 'KOLOROWA'})
-        tds = table.find_all('td', class_='BIALA')
-
-        i = 0
-
-        messages_datas = []
-        temp_data = {}
-        for td in tds:
-            if i == 1:
-                temp_data['sender'] = td.text.strip()
-            if i == 2:
-                temp_data['title'] = td.text.strip()
-                row_id = re.findall(r'rowId=(.*)&',td.find('a').attrs['href'])[0]
-                temp_data['message'] = get_mail_content(row_id)
-            if i == 3:
-                temp_data['priority'] = td.text.strip()
-            if i == 4:
-                temp_data['date'] = td.text.strip()
-
-            i += 1
-            if i % 5 == 0:
-                i = 0
-                messages_datas.append(temp_data)
-                temp_data = {}
-
-        return messages_datas
-
     if max_mails == -1:
         max_mails = last_page_num * 5 + 5
 
     max_index = min(last_page_num * 5, max_mails)
+    indexes = range(0, max_index, 5)
 
     flatten = lambda t: [item for sublist in t for item in sublist]
-    mails = flatten([get_five_mails(i) for i in range(0, max_index, 5)])[:max_mails]
-    return mails
+
+    pool = multiprocessing.Pool(processes=cpus)
+    fetched_mails = pool.starmap(get_five_mails, zip(indexes, repeat(s), repeat(web_token), repeat(web_session_token)))
+
+    result_mails = flatten(fetched_mails)[:max_mails]
+
+    return result_mails
+
+def get_all_mails(login, password):
+    return get_mails(login, password, -1)
--- test.py
+++ test.py
@@ -1,17 +1,24 @@
 import os
 import pprint
 from app.edu_cl_mail import get_all_mails, get_mails
+import time
+
 
 from dotenv import load_dotenv
-load_dotenv()
 
-print(f'Logging in {os.environ["LOGIN"]}')
+if __name__ == "__main__":
+    load_dotenv()
 
-# all_mails = get_all_mails(os.environ['LOGIN'], os.environ['PASSWORD'])
-# pprint.pprint(all_mails)
-# print(len(all_mails))
+    print(f'Logging in {os.environ["LOGIN"]}')
 
-mails = get_mails(os.environ['LOGIN'], os.environ['PASSWORD'], 5)
-# pprint.pprint(mails)
-print(mails[2]['message'])
-# print(len(mails))
+    # all_mails = get_all_mails(os.environ['LOGIN'], os.environ['PASSWORD'])
+
+    start = time.time()
+    # mails = get_mails(os.environ['LOGIN'], os.environ['PASSWORD'], 50)
+    mails = get_all_mails(os.environ['LOGIN'], os.environ['PASSWORD'])
+    end = time.time()
+    print("Finished fetching mails, time:")
+    print(end - start)
+    # pprint.pprint(mails)
+    # print(mails[2]['message'])
+    print(len(mails))
