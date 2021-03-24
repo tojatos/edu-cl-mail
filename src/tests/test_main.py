94bd7015b7235ea6dba17f583b01ed1e3e13802d
tojatos <tojatos@gmail.com>
2021-03-25 00:39:56 +0100

    pytest init; folder structure

---
+++ src/tests/test_main.py
@@ -0,0 +1,17 @@
+
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+
+# def test_read_main():
+#     response = client.get("/")
+#     assert response.status_code == 200
+#     assert response.json() == {"message": "Hello World"}
+
+def test_login_check():
+    response = client.post("/api/login_check")
+    assert response.status_code == 200
+    assert response.json() == {"message": "true"}
