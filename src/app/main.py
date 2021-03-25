801a4c5ebfd36a099df6ed5f48064a44e6464d61
tojatos <tojatos@gmail.com>
2021-03-25 02:21:36 +0100

    Dockerfile working; login check added

--- src/app/main.py
+++ src/app/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from .edu_cl_mail import check_login
+
+
 class Credentials(BaseModel):
     username: str
     password: str
+
+
+# class LoginCheckResult(BaseModel):
+#     authenticated: bool
 
 app = FastAPI()
 
@@ -11,3 +18,9 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/api/login_check")
+def login_check(c: Credentials):
+    """ returns "true", "false" or "invalid credentials or internal error" """
+    return check_login(c.username, c.password)
--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,9 +1,22 @@
+import os
 
 from fastapi.testclient import TestClient
-
 from app.main import app
+from dotenv import load_dotenv
 
 client = TestClient(app)
+load_dotenv()
+
+
+def get_env(key, fallback):
+    try:
+        return os.environ[key]
+    except KeyError:
+        return fallback
+
+
+USERNAME = get_env('LOGIN', '')
+PASSWORD = get_env('PASSWORD', '')
 
 
 # def test_read_main():
@@ -12,6 +25,7 @@ client = TestClient(app)
 #     assert response.json() == {"message": "Hello World"}
 
 def test_login_check():
-    response = client.post("/api/login_check")
+    response = client.post("/api/login_check",
+                           json={"username": USERNAME, "password": PASSWORD})
     assert response.status_code == 200
-    assert response.json() == {"message": "true"}
+    assert response.json() == True
