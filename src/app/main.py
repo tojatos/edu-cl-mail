0a0e93a25a99a0e7d24feeb7e74fff25b502a1f8
tojatos <tojatos@gmail.com>
2021-03-26 00:29:19 +0100

    login check improved

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 from .edu_cl_mail import check_login
@@ -9,8 +9,9 @@ class Credentials(BaseModel):
     password: str
 
 
-# class LoginCheckResult(BaseModel):
-#     authenticated: bool
+
+class LoginCheckResult(BaseModel):
+    authenticated: bool
 
 app = FastAPI()
 
@@ -20,7 +21,10 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.post("/api/login_check")
+@app.post("/api/login_check", response_model=LoginCheckResult)
 def login_check(c: Credentials):
     """ returns "true", "false" or "invalid credentials or internal error" """
-    return check_login(c.username, c.password)
+    login_check_result = check_login(c.username, c.password)
+    if not login_check_result:
+        raise HTTPException(status_code=401, detail="invalid credentials or internal error")
+    return {"authenticated": login_check_result}
--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -24,8 +24,20 @@ PASSWORD = get_env('PASSWORD', '')
 #     assert response.status_code == 200
 #     assert response.json() == {"message": "Hello World"}
 
+def test_login_check_invalid_request():
+    response = client.post("/api/login_check",
+                           json={"usernames": "w"})
+    assert response.status_code == 422
+
+
+def test_login_check_invalid_credentials():
+    response = client.post("/api/login_check",
+                           json={"username": USERNAME, "password": "123"})
+    assert response.status_code == 401
+
+
 def test_login_check():
     response = client.post("/api/login_check",
                            json={"username": USERNAME, "password": PASSWORD})
     assert response.status_code == 200
-    assert response.json() == True
+    assert response.json()["authenticated"] == True
